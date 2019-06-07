92178babe19e2826d848b9028e5d45d4289f6f81
RaulMirandaOSF <raul.miranda@osf-commerce.com>
2019-06-07 09:56:42 -0300

    Html added to all components

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,11 @@ import './styles/global.css';
 
 import Header from './components/Header';
 // import Courses from './components/Courses';
-import CourseDetail from './components/CourseDetail';
+// import CourseDetail from './components/CourseDetail';
+import UserSignIn from './components/UserSignIn';
+// import UserSignUp from './components/UserSignUp';
+// import CreateCourse from './components/CreateCourse';
+// import UpdateCourse from './components/UpdateCourse';
 
 class App extends Component {
 
@@ -40,14 +44,18 @@ class App extends Component {
 
   render() {
 
-    const course = this.state.courses[0] && <CourseDetail course={this.state.courses[0]}/>;
+    // const course = this.state.courses[0] && <CourseDetail course={this.state.courses[0]}/>;
 
     return (
       <div className="App">
         <Header />
         <hr />
         {/* <Courses courses={this.state.courses}/> */}
-        { course }
+        {/* { course } */}
+        <UserSignIn/>
+        {/* <UserSignUp/> */}
+        {/* <CreateCourse/> */}
+        {/* <UpdateCourse/> */}
       </div>
     );
   }
--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -3,7 +3,20 @@ import React from 'react';
 const UserSignIn = (props) => {
 
     return (
-        <h4>UserSignIn component</h4>
+        <div className="bounds">
+            <div className="grid-33 centered signin">
+                <h1>Sign In</h1>
+                <div>
+                    <form>
+                        <div><input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" value=""/></div>
+                        <div><input id="password" name="password" type="password" className="" placeholder="Password" value=""/></div>
+                        <div className="grid-100 pad-bottom"><button className="button" type="submit">Sign In</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button></div>
+                    </form>
+                </div>
+                <p>&nbsp;</p>
+                <p>Don't have a user account? <a href="sign-up.html">Click here</a> to sign up!</p>
+            </div>
+        </div>
     );
 }
 
